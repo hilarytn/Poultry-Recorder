d43d596cb1e0fc34b2c2af7520da9adb1e6e71f1
Hilary Titus Naor <47435436+hilarytn@users.noreply.github.com>
2024-11-01 07:23:13 +0000

    User model methods: set_password and check_password

--- models/user.py
+++ models/user.py
@@ -11,3 +11,19 @@ class User(db.Model):
     password_hash = db.Column(db.String(255), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.now(timezone.utc))
     updated_at = db.Column(db.DateTime, onupdate=datetime.now(timezone.utc))
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'fullname': self.fullname,
+            'email': self.email,
+        }
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+    
+    def __repr__(self):
+        return f'<User {self.username}>'
